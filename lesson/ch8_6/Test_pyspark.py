88db08f822147d79988c7224c9151bffd42a264f
kyun-9458 <jkk9458@gmail.com>
2025-06-02 15:26:10 +0900

    fixing

--- lesson/ch8_6/Test_pyspark.py
+++ lesson/ch8_6/Test_pyspark.py
@@ -3,15 +3,15 @@ import time
 
 spark = SparkSession \
     .builder \
-    .appName('TEST_JKK') \
+    .appName('Test_pyspark') \
     .getOrCreate()
 
 schema = 'NAME STRING, REGION STRING, AGE INT, GF BOOLEAN'
 fr = spark.createDataFrame(data=[('KK', 29, True), ('JS', 29, False), ('MJ', 30, False), ('JM', 29, False), ('TwoWeeks Dragon', 29, True)], schema = schema)
 fr.show()
 
-schema2 = 'NAME STRING, AGE INT, MARRIAGE BOOLEAN'
-df = spark.createDataFrame(data=[('hong',28, False),('kim',33,True)], schema=schema2)
+schemaa = 'NAME STRING, AGE INT, MARRIAGE BOOLEAN'
+df = spark.createDataFrame(data=[('hong',28, False),('kim',33,True)], schema=schemaa)
 df.show()
 
 time.sleep(60)
